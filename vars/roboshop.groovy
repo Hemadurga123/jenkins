c3b671d1dbf4b7f3e4c4e4fb9c60563ed3b87390
hema <Hemadurga@zelarsoft.com>
2021-06-29 17:47:01 +0530

    1

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -1,6 +1,21 @@
-def call() {
+def call(Map params = [:]) {
+  // Start Default Arguments
+  def args = [
+          NEXUS_IP         : '172.31.10.228',
+
+  ]
+  args << params
+
+  // End Default + Required Arguments
   pipeline{
     agent  any
+
+    environment {
+      COMPONENT = "${args.COMPONENT}"
+      NEXUS_IP = "${args.NEXUS_IP"
+      PROJECT_NAME = "${args.PROJECT_NAME}"
+    }
+
 
     stages {
       stage ('prepare  the  Artifact') {
@@ -13,7 +28,7 @@ def call() {
       stage('upload artifact'){
         steps{
           sh '''
-          curl -f -v -u admin:admin123 --upload-file ../frontend.zip http://172.31.10.228:8081/repository/frontend/frontend.zip
+          curl -f -v -u admin:admin123 --upload-file ../frontend.zip http://${NEXUS_IP}:8081/repository/frontend/frontend.zip
 
         '''
 
