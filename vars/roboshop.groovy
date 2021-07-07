042e58102b4d25280bef7320507a22a8c91beac3
hema <Hemadurga@zelarsoft.com>
2021-07-07 16:14:51 +0530

    1

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -14,6 +14,8 @@ def call(Map params = [:]) {
       COMPONENT = "${args.COMPONENT}"
       NEXUS_IP = "${args.NEXUS_IP}"
       PROJECT_NAME = "${args.PROJECT_NAME}"
+      SLAVE_LABEL :  "${args.SLAVE_LABEL}"
+
     }
 
 
