c26b966bac585898f2414828323744e03255bb33
hema <Hemadurga@zelarsoft.com>
2021-07-08 17:17:58 +0530

    1

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -2,7 +2,7 @@ def call(Map params = [:]) {
   // Start Default Arguments
   def args = [
           NEXUS_IP : '172.31.10.228',
-          SLAVE_LABEL : 'JAVA'
+          SLAVE_LABEL : 'JAVA',
 
   ]
   args << params
@@ -24,7 +24,7 @@ def call(Map params = [:]) {
       stage('prepare  the  Artifact') {
         steps {
           sh '''
-         zip -r login.zip *
+         zip -r ../login.zip *
        '''
         }
       }
