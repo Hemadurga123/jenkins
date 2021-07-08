ed058c8dd0e62b28b5ee8beacd222095a2f16074
hema <Hemadurga@zelarsoft.com>
2021-07-08 17:07:45 +0530

    1

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -1,7 +1,8 @@
 def call(Map params = [:]) {
   // Start Default Arguments
   def args = [
-          NEXUS_IP: '172.31.10.228',
+          NEXUS_IP : '172.31.10.228',
+          SLAVE_LABEL : 'JAVA'
 
   ]
   args << params
