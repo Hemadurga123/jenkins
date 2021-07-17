b95c48f620d4b26b7c490b0d6784fd55f007b1d5
Hemadurga123 <82637654+Hemadurga123@users.noreply.github.com>
2021-07-17 12:06:41 +0530

    Update roboshop.groovy

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -1,90 +1,62 @@
+  
 def call(Map params = [:]) {
-  // Start Default Arguments
-  def args = [
-          NEXUS_IP   : '172.31.10.228',
-          SLAVE_LABEL: 'NODEJS',
+    // Start Default Arguments
+    def args = [
+            NEXUS_IP               : '172.31.10.228',
+    ]
+    args << params
 
-  ]
-  args << params
+    // End Default + Required Arguments
+    pipeline {
+        agent {
+            label "${args.SLAVE_LABEL}"
+        }
 
-  // End Default + Required Arguments
-  pipeline {
-    agent {
-      label "${args.SLAVE_LABEL}"
+        triggers {
+            pollSCM('* * * * 1-5')
+        }
+
+        environment {
+            COMPONENT     = "${args.COMPONENT}"
+            NEXUS_IP      = "${args.NEXUS_IP}"
+            PROJECT_NAME  = "${args.PROJECT_NAME}"
+            SLAVE_LABEL   = "${args.SLAVE_LABEL}"
+            APP_TYPE      = "${args.APP_TYPE}"
+        }
+
+        stages {
+
+            stage('Build Code & Install Dependencies') {
+                steps {
+                    script {
+                        build = new nexus()
+                        build.code_build("${APP_TYPE}", "${COMPONENT}")
+                    }
+                }
+            }
+
+
+            stage('Prepare Artifacts') {
+                steps {
+                    script {
+                        prepare = new nexus()
+                        prepare.make_artifacts("${APP_TYPE}", "${COMPONENT}")
+                    }
+                }
+            }
+
+            stage('Upload Artifacts') {
+                steps {
+                    script {
+                        prepare = new nexus()
+                        prepare.nexus(COMPONENT)
+                    }
+                }
+            }
+
+
+
+        }
+
     }
-
-    environment {
-     // COMPONENT = "${args.COMPONENT}"
-      NEXUS_IP = "${args.NEXUS_IP}"
-      PROJECT_NAME = "${args.PROJECT_NAME}"
-      SLAVE_LABEL = "${args.SLAVE_LABEL}"
-      APP_TYPE = "${args.APP_TYPE}"
-
-    }
-
-
-    stages {
-      stage('prepare  the  Artifact ------NGINX') {
-        when{
-          environment name: 'APP_TYPE', value: 'JAVA'
-
-        }
-        steps {
-          script{
-            prepare = new nexus()
-            prepare.make_artifacts 'frontend'
-
-          }
-          sh '''
-          ls 
-       '''
-        }
-      }
-      stage('prepare  the  Artifact ------login') {
-        when{
-          environment name: 'COMPONENT', value: 'login'
-
-        }
-        steps {
-          script{
-            prepare = new nexus()
-            prepare.make_artifacts 'login'
-          }
-          sh '''
-         ls
-       '''
-        }
-      }
-      stage('prepare  the  Artifact ------users') {
-        when{
-          environment name: 'COMPONENT', value: 'users'
-
-        }
-        steps {
-          script{
-            prepare = new nexus()
-            prepare.make_artifacts 'users'
-          }
-          sh '''
-          ls
-       '''
-        }
-      }
-      stage('prepare  the  Artifact ------todo') {
-        when{
-          environment name: 'COMPONENT', value: 'todo'
-
-        }
-        steps {
-          script{
-            prepare = new nexus ()
-            prepare.make_artifacts 'todo'
-          }
-          sh '''
-          ls
-       '''
-        }
-      }
-    }
-  }
 }
