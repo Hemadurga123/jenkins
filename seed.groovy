b6bffc152afa5388c283b0422fc346d7352bbe72
hema <Hemadurga@zelarsoft.com>
2021-07-18 12:04:25 +0530

    1

--- seed.groovy
+++ seed.groovy
@@ -23,12 +23,12 @@ for (i in 0..count) {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
               'url'('https://github.com/Hemadurga123/'+j+'.git')
-              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+              'refspec'('\'+refs/tags/*\':\'refs/remotes/main/tags/*\'')
             }
           }
           'branches' {
             'hudson.plugins.git.BranchSpec' {
-              'name'('main')
+              'name'('*/tags/*')
             }
           }
         }
