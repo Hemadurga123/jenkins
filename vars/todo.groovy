9f565a3690c53fd4d4dfa328312c36120ed2fc63
hema <Hemadurga@zelarsoft.com>
2021-07-22 14:15:14 +0530

    update-1

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -2,7 +2,7 @@
 def call(Map params = [:]) {
     // Start Default Arguments
     def args = [
-            NEXUS_IP               : '172.31.10.228',
+            NEXUS_IP               : '172.31.8.222',
     ]
     args << params
 
